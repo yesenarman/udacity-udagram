0305376b22c576a81a7febfd506bb6d25c2b4ae1
Arman Yessenamanov <yesenarman@gmail.com>
2020-06-01 01:08:40 +0600

    chore: Add reverse proxy service and docker files

--- udagram-feed-api/src/server.ts
+++ udagram-feed-api/src/server.ts
@@ -14,7 +14,7 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 8081;
 
   app.use(bodyParser.json());
 
--- udagram-users-api/src/server.ts
+++ udagram-users-api/src/server.ts
@@ -14,7 +14,7 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.PORT || 8081;
+  const port = process.env.PORT || 8082;
 
   app.use(bodyParser.json());
 
