e9b2e277cf7b5b2a97480b605fd7e7623635fb75
Arman Yessenamanov <yesenarman@gmail.com>
2020-06-01 02:57:53 +0600

    chore: Add health check endpoints to backend services

--- udagram-feed-api/src/server.ts
+++ udagram-feed-api/src/server.ts
@@ -41,6 +41,11 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
   app.use('/api/v0/', IndexRouter);
 
+  // Health check
+  app.get( '/health', async ( req, res ) => {
+    res.send();
+  } );
+
   // Root URI call
   app.get( '/', async ( req, res ) => {
     res.send( '/api/v0/' );
